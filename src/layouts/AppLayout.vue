1f0f37656a30fc5f2d52f6822dfa2b49f16c9b7c
Nahuel Fernandez <nahuefer105@outlook.com>
2023-06-05 14:01:30 -0300

    Added layouts

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid min-h-screen h-full p-3 place-items-center bg-gray-100">
+        <div>
+            <the-header></the-header>
+            <div class="bg-white border rounded-lg p-3 shadow-sm min-h-[10rem]">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TheHeader from '@/components/layout/TheHeader.vue'
+
+export default {
+    components: {
+        TheHeader,
+    },
+};
+</script>
---
+++ src/layouts/Page404Layout.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="grid h-screen place-items-center bg-gray-100">
+        Not found 
+    </div>
+</template>
